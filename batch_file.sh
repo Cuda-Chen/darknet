8f53a17de1e6831640c86660e9dd6527b9132995
Cuda-Chen <clh960524@gmail.com>
2018-12-14 15:01:36 +0800

    version 1 of batch_file.sh

--- batch_file.sh
+++ batch_file.sh
@@ -7,6 +7,17 @@ counter=0
 while read line
 do
 	echo $line
+	./darknet detector test ./fish/cfg/obj.data ./fish/cfg/yolov3.cfg fish_train.backup $line
+	cd '1A_result'
+	mkdir $counter
+	cd $counter
+	cp ../../*.jpg .
+	cd ../../
+
 	counter=$((counter+1))
-	echo $counter
+	#echo $counter
+	#if [ $counter -gt 2 ]; then
+	#	echo "get the hell out"
+	#	break
+	#fi
 done
